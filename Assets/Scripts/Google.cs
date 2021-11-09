c7141d6bcde05d4be591d186d64533683ff98afe
Andries <61500390+Emerald001@users.noreply.github.com>
2021-11-09 10:33:05 +0100

    Start of google en scroll

---
+++ Assets/Scripts/Google.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Google : MonoBehaviour
+{
+    public InputField input;
+
+    private void Update() {
+        if(input.text == "Book") {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Tmp.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tmp : MonoBehaviour
+{
+    private float size;
+    private float top;
+    private RectTransform rt;
+
+    // Start is called before the first frame update
+    void Start() { 
+        rt = GetComponent<RectTransform>();
+
+        top = rt.position.y;
+        size = rt.sizeDelta.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var scroll = -Input.mouseScrollDelta;
+        var Vpos = rt.position.y + (scroll.y * 100);
+        Vpos = Mathf.Clamp(Vpos, top, top + size/2);
+
+        Debug.Log(Vpos);
+
+        rt.position = new Vector2(transform.position.x, Vpos);
+    }
+}
